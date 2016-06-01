7701ddb989ba72304bc3e74439eb5ec7d50e2ce7
Suyana Lozada <slozadag@gmail.com>
2016-05-31 19:24:51 -0700

    TSP utility functions

--- tsp.py
+++ tsp.py
@@ -21,17 +21,17 @@ def cartesian_matrix(cities):
     #Takes a Python list of (x,y) tuples and outputs a Python dictionary that contains the distance
     #between the distances between any two cities
 	matrix={}
-    for i,(x1,y1) in enumerate(cities):
-        for j,(x2,y2) in enumerate(cities):
-            dx,dy=x1-x2,y1-y2
-            dist=int(round(math.sqrt(dx*dx + dy*dy)))
-            matrix[i,j]=dist
-    return matrix
+	for i,(x1,y1) in enumerate(cities):
+		for j,(x2,y2) in enumerate(cities):
+			dx,dy=x1-x2,y1-y2
+			dist=int(round(math.sqrt(dx*dx + dy*dy)))
+			matrix[i,j]=dist
+	return matrix
 
 
 filename="tsp_example_1.txt" 
 cities=readinstance(filename)
-print cartesian_matrix(cities);
+print cartesian_matrix(cities)
 
 
 
