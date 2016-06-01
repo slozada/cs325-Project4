e22860d0219c01a80f9aadfaa1231834b12e4ca9
Suyana Lozada <slozadag@gmail.com>
2016-05-31 19:21:05 -0700

    TSP utility functions

--- tsp.py
+++ tsp.py
@@ -6,8 +6,6 @@ def readinstance(filename):
     # each line of input file represents a city given by three integers:
     # identifier x-coordinate y-coordinate (space separated)
     # city identifiers are always consecutive integers starting with 0
-    # (although this is not assumed here explicitly,
-    #    it will be a requirement to match up with the solution file)
 	f = open(filename,'r')
 	line = f.readline()
 	cities = []
@@ -18,10 +16,22 @@ def readinstance(filename):
 	f.close()
 	return cities
 
+def cartesian_matrix(cities):
+    #create a distance matrix for the city coords
+    #Takes a Python list of (x,y) tuples and outputs a Python dictionary that contains the distance
+    #between the distances between any two cities
+	matrix={}
+    for i,(x1,y1) in enumerate(cities):
+        for j,(x2,y2) in enumerate(cities):
+            dx,dy=x1-x2,y1-y2
+            dist=int(round(math.sqrt(dx*dx + dy*dy)))
+            matrix[i,j]=dist
+    return matrix
+
 
 filename="tsp_example_1.txt" 
-print readinstance(filename)
-
+cities=readinstance(filename)
+print cartesian_matrix(cities);
 
 
 
